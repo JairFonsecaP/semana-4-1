3252d0041f0d20f67d23e9cca86ccb2b328d7bcd
JairFonsecaP <jairfonseca64@yahoo.esa>
2020-12-21 13:20:22 -0300

    sin autenticacion para pruebas

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -5,10 +5,10 @@ const auth = require("../../middlewares/auth.js");
 //api/user/
 
 router.post("/login", userController.login);
-router.get("/list", auth.verifyUsuario, userController.list);
-router.post("/add", auth.verifyUsuario, userController.add);
-router.put("/update", auth.verifyUsuario, userController.update);
-router.put("/activate", auth.verifyUsuario, userController.activate);
-router.put("/deactivate", auth.verifyUsuario, userController.deactivate);
+router.get("/list", userController.list);
+router.post("/add", userController.add);
+router.put("/update", userController.update);
+router.put("/activate", userController.activate);
+router.put("/deactivate", userController.deactivate);
 
 module.exports = router;
